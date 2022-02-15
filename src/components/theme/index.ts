9bdb5199d9b99229f62e1415247dd731bea1d816
George Perry <gmperry18@gmail.com>
2022-02-15 12:15:48 +0000

    Added breakpoints and new themes

--- src/components/theme/index.ts
+++ src/components/theme/index.ts
@@ -1,8 +1,29 @@
 import { DefaultTheme } from 'styled-components';
 
 interface ThemeColors {
-  primary: string;
-  secondary: string;
+  fullBlack: string;
+  fullWhite: string;
+  offBlack: string;
+  offWhite: string;
+  primaryNeon: string;
+  primaryPastel: string;
+
+  grey01: string;
+  grey02: string;
+  grey03: string;
+  grey04: string;
+  grey05: string;
+  grey06: string;
+
+  secondaryNeon01: string;
+  secondaryNeon02: string;
+  secondaryNeon03: string;
+  secondaryNeon04: string;
+
+  secondaryPastel01: string;
+  secondaryPastel02: string;
+  secondaryPastel03: string;
+  secondaryPastel04: string;
 }
 
 interface CustomTheme {
@@ -16,29 +37,47 @@ declare module 'styled-components' {
     space: Array<number>;
     fontSizes: Array<number>;
     radii: Array<number>;
+    breakpoints: {
+      [name in 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl']: number;
+    };
   }
 }
 
-export const lightTheme: DefaultTheme = {
-  name: 'light',
-  space: [0, 2, 4, 8, 16, 32],
+export const defaultTheme: DefaultTheme = {
+  name: 'Default',
+  space: [0, 2, 4, 8, 16, 32, 64],
   colors: {
-    primary: '#000000',
-    secondary: '#ffffff',
+    fullBlack: '#000000',
+    fullWhite: '#ffffff',
+    offBlack: '#111111',
+    offWhite: '#F4F4F4',
+    primaryNeon: '#F0F01F',
+    primaryPastel: '#EDD10F',
+    grey01: '#333333',
+    grey02: '#4F4F4F',
+    grey03: '#828282',
+    grey04: '#BDBDBD',
+    grey05: '#E0E0E0',
+    grey06: '#F2F2F2',
+    secondaryNeon01: '#ED592B',
+    secondaryNeon02: '#0DB291',
+    secondaryNeon03: '#E04F96',
+    secondaryNeon04: '#0042F7',
+    secondaryPastel01: '#E5A321',
+    secondaryPastel02: '#8CC9A8',
+    secondaryPastel03: '#E380B0',
+    secondaryPastel04: '#3861D4',
   },
-  fonts: ['sans-serif', 'Roboto'],
+  fonts: ['GeorgioSansHeavy', 'Inter'],
   fontSizes: [12, 14, 16, 18, 24, 32],
   radii: [0, 2, 4, 8],
-};
-
-export const darkTheme: DefaultTheme = {
-  name: 'dark',
-  space: [0, 2, 4, 8, 16, 32],
-  colors: {
-    primary: '#000000',
-    secondary: '#ffffff',
+  breakpoints: {
+    xxs: 320,
+    xs: 375,
+    sm: 425,
+    md: 768,
+    lg: 1024,
+    xl: 1440,
+    xxl: 2560,
   },
-  fonts: ['sans-serif', 'Roboto'],
-  fontSizes: [12, 14, 16, 18, 24, 32],
-  radii: [0, 2, 4, 8],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+export { default as Button } from './components/atoms/Button';
+export { default as Spacer } from './components/atoms/Spacer';
 export { default as ThemeProvider } from './components/theme/ThemeProvider';
-export { lightTheme, darkTheme } from './components/theme/themes';
