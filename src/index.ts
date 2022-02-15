9730ead33e9d9d34893d38bdea24394ff6e34c7c
George Perry <gmperry18@gmail.com>
2022-02-15 15:45:13 +0000

    Added defaultTheme to src export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './components/atoms/Button';
 export { default as Spacer } from './components/atoms/Spacer';
+export { defaultTheme } from './components/theme';
 export { default as ThemeProvider } from './components/theme/ThemeProvider';
